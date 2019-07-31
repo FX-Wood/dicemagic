5989cc6fc240406a276c672b4a234c7681c3ba8f
FX-Wood <fxwood2@gmail.com>
2019-07-31 16:15:49 -0700

    Prints dice info to stdout

--- chat-clients/rest.go
+++ chat-clients/rest.go
@@ -23,6 +23,23 @@ type RESTRollRequest struct {
 	Probability bool   `json:"with_probability,omitempty"`
 }
 
+type Dice struct {
+	Count                int64             `protobuf:"varint,1,opt,name=Count,proto3" json:"Count,omitempty"`
+	Sides                int64             `protobuf:"varint,2,opt,name=Sides,proto3" json:"Sides,omitempty"`
+	Total                int64             `protobuf:"varint,3,opt,name=Total,proto3" json:"Total,omitempty"`
+	Faces                []int64           `protobuf:"varint,4,rep,packed,name=Faces,proto3" json:"Faces,omitempty"`
+	Color                string            `protobuf:"bytes,5,opt,name=Color,proto3" json:"Color,omitempty"`
+	Max                  int64             `protobuf:"varint,6,opt,name=Max,proto3" json:"Max,omitempty"`
+	Min                  int64             `protobuf:"varint,7,opt,name=Min,proto3" json:"Min,omitempty"`
+	DropHighest          int64             `protobuf:"varint,8,opt,name=DropHighest,proto3" json:"DropHighest,omitempty"`
+	DropLowest           int64             `protobuf:"varint,9,opt,name=DropLowest,proto3" json:"DropLowest,omitempty"`
+	Chart                []byte            `protobuf:"bytes,10,opt,name=Chart,proto3" json:"Chart,omitempty"`
+	Probabilities        map[int64]float64 `protobuf:"bytes,11,rep,name=Probabilities,proto3" json:"Probabilities,omitempty" protobuf_key:"varint,1,opt,name=key,proto3" protobuf_val:"fixed64,2,opt,name=value,proto3"`
+	XXX_NoUnkeyedLiteral struct{}          `json:"-"`
+	XXX_unrecognized     []byte            `json:"-"`
+	XXX_sizecache        int32             `json:"-"`
+}
+
 func RESTRollHandler(e interface{}, w http.ResponseWriter, r *http.Request) error {
 	env, _ := e.(*env)
 	log := env.log.WithRequest(r)
@@ -61,9 +78,12 @@ func RESTRollHandler(e interface{}, w http.ResponseWriter, r *http.Request) erro
 
 func StringFromRollResponse(rr *pb.RollResponse) string {
 	var s []string
+	fmt.Printf("\nROLL RESPONSE\n", rr.String())
 	for _, ds := range rr.DiceSets {
+		fmt.Printf("\nDICE SET\n", ds.String())
 		var faces []interface{}
 		for _, d := range ds.Dice {
+			fmt.Printf("\nDICE\n", d.String())
 			faces = append(faces, facesSliceString(d.Faces))
 		}
 		s = append(s, fmt.Sprintf("%s = *%s*", fmt.Sprintf(ds.ReString, faces...), strconv.FormatInt(ds.Total, 10)))
