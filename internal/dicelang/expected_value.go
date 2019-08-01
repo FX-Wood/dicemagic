eb8632be93904e566da69fe8b81b50b99b0bd46d
FX-Wood <fxwood2@gmail.com>
2019-07-31 21:45:28 -0700

    Fixes scoping of types

--- internal/dicelang/expected_value.go
+++ internal/dicelang/expected_value.go
@@ -1,12 +1,10 @@
 package dicelang
 
-type pmf map[int64]float64 // in our case the pmf maps the integer result to probability as a percentage
-
 //ExpectedValue takes a probability mass function(pmf) and returns an expected value(eX)
-func ExpectedValue(pmf) float64 {
+func ExpectedValue(pmf map[int64]float64) float64 {
 	eX := 0.0
 	for value, probability := range pmf {
-		eX += (float64(value) * probability) / 100
+		eX += (float64(value) * probability) / 100 // in our case the pmf maps the integer result to probability as a percentage
 	}
 	return eX
 }
